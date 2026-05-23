1cafc35dbe1a21e76e52b6e8c916c60ccdf610f6
Himesh A <83563033+meshhi13@users.noreply.github.com>
2026-05-23 14:03:55 -0400

    Add: surrounded-regions - submission-5

---
+++ Data Structures & Algorithms/surrounded-regions/submission-5.py
@@ -0,0 +1,49 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        def dfs(x, y):
+            queue = []
+            queue.append((x, y))
+            visited = set()
+
+            while queue:
+                i, j = queue.pop()
+                if (i, j) in visited:
+                    continue
+                visited.add((i, j))
+                board[i][j] = "T"
+                
+                if i > 0:
+                    if board[i - 1][j] == "O":
+                        queue.append((i - 1, j))
+                if i < len(board) - 1:
+                    if board[i + 1][j] == "O":
+                        queue.append((i + 1, j))
+                if j > 0:
+                    if board[i][j - 1] == "O":
+                        queue.append((i, j - 1))
+                if j < len(board[0]) - 1:
+                    if board[i][j + 1] == "O":
+                        queue.append((i, j + 1))
+
+
+        for i in range(len(board)):
+            if board[i][0] == "O":
+                dfs(i, 0)
+            if board[i][len(board[0]) - 1] == "O":
+                dfs(i, len(board[0]) - 1)
+        
+        for i in range(len(board[0])):
+            if board[0][i] == "O":
+                dfs(0, i)
+            if board[len(board) - 1][i] == "O":
+                dfs(len(board) - 1, i)
+
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if board[i][j] == "O":
+                    board[i][j] = "X"
+                if board[i][j] == "T":
+                    board[i][j] = "O"
+                
+
+        
